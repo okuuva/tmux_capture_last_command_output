1be4b115c3305052d4dda6a82d89535aec26ee70
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-04 08:06:07 +0200

    feat: handle multiline prompts

--- plugin.sh
+++ plugin.sh
@@ -2,9 +2,11 @@
 
 PROMPT_PATTERN=$(tmux show-option -gqv @command-capture-prompt-pattern)
 PROMPT_PATTERN=${PROMPT_PATTERN:-" ] % "}
+PROMPT_LINES=$(tmux show-option -gqv @command-capture-prompt-lines)
+PROMPT_LINES=${PROMPT_LINES:-1}
 EDITOR_CMD=$(tmux show-option -gqv @command-capture-editor-cmd)
 EDITOR_CMD=${EDITOR_CMD:-"$EDITOR -"}
 
-result=$(tmux capture-pane -pJS - | tac | sed -e "0,/$PROMPT_PATTERN/d" | sed "/$PROMPT_PATTERN/,\$d" | tac)
+result=$(tmux capture-pane -pJS - | tac | sed -n "1,${PROMPT_LINES}d; /$PROMPT_PATTERN/q; p" | tac)
 
 tmux new-window -n last-command-output -e result="$result" "echo \"$result\" | $EDITOR_CMD"
