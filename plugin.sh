24f90cd1ca8493c53237ab344e54201bdb0da872
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-04 08:15:35 +0200

    feat: make capture-pane flags configurable

--- plugin.sh
+++ plugin.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-PROMPT_PATTERN=$(tmux show-option -gqv @command-capture-prompt-pattern)
+PROMPT_PATTERN=$(tmux show-option -gqv "@command-capture-prompt-pattern")
 PROMPT_PATTERN=${PROMPT_PATTERN:-" ] % "}
-PROMPT_LINES=$(tmux show-option -gqv @command-capture-prompt-lines)
+PROMPT_LINES=$(tmux show-option -gqv "@command-capture-prompt-lines")
 PROMPT_LINES=${PROMPT_LINES:-1}
-EDITOR_CMD=$(tmux show-option -gqv @command-capture-editor-cmd)
+CAPTURE_PANE_FLAGS=$(tmux show-option -gqv "@command-capture-flags")
+CAPTURE_PANE_FLAGS=${CAPTURE_PANE_FLAGS:-"-pJS -"}
+EDITOR_CMD=$(tmux show-option -gqv "@command-capture-editor-cmd")
 EDITOR_CMD=${EDITOR_CMD:-"$EDITOR -"}
 
-result=$(tmux capture-pane -pJS - | tac | sed -n "1,${PROMPT_LINES}d; /$PROMPT_PATTERN/q; p" | tac)
+result=$(tmux capture-pane "$CAPTURE_PANE_FLAGS" | tac | sed -n "1,${PROMPT_LINES}d; /$PROMPT_PATTERN/q; p" | tac)
 
 tmux new-window -n last-command-output "echo \"$result\" | $EDITOR_CMD"
