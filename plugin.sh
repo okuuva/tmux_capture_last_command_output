031300dd2410aa93db585112441ae8b5dc06470a
Oula Kuuva <oula.kuuva@iki.fi>
2025-02-04 21:07:35 +0200

    feat: check if tac is available
    tac isn't available by default on macOS. If the command is missing the
    script fails silently which is very confusing. It's better to check if
    the command is available first and display an error message saying that
    coreutils are needed so the user is pointed to the right direction.

--- plugin.sh
+++ plugin.sh
@@ -9,6 +9,12 @@ CAPTURE_PANE_FLAGS=${CAPTURE_PANE_FLAGS:-"-pJS -"}
 EDITOR_CMD=$(tmux show-option -gqv "@command-capture-editor-cmd")
 EDITOR_CMD=${EDITOR_CMD:-"$EDITOR -"}
 
+# Check first if tac is available (it isn't by default on macOS)
+if ! command -v tac >/dev/null 2>&1; then
+	tmux display-message -d 0 "tmux_capture_last_command_output: Command 'tac' not found. Make sure you have coreutils installed."
+	exit 0
+fi
+
 # We want the flags to split, otherwise they won't work
 # shellcheck disable=SC2086
 result=$(tmux capture-pane $CAPTURE_PANE_FLAGS | tac | sed -n "1,${PROMPT_LINES}d; /$PROMPT_PATTERN/q; p" | tac)
